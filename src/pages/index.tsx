5f67886567e41cb2544f1282c46cc63c2383fdc9
thal0x <thal0x.eth@gmail.com>
2023-07-20 22:21:42 +0200

    add comparison chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -177,7 +177,7 @@ export default function Home() {
   return (
     <Layout>
       {fetchStatus === "fetching" && (
-        <div className="w-full h-1 bg-indigo-500 absolute top-0 left-0 right-0 overflow-hidden">
+        <div className="w-full h-1 bg-indigo-500 fixed top-0 left-0 right-0 overflow-hidden">
           <div className="bg-indigo-600 w-full h-full animate-fade-right animate-infinite"></div>
         </div>
       )}
