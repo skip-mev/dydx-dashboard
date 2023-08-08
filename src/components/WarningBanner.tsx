7f53ce1006ae92b857a44e5226e466a72af23f3c
Zygimantass <5236121+Zygimantass@users.noreply.github.com>
2023-08-08 18:36:54 +0200

    fix: center banner

--- src/components/WarningBanner.tsx
+++ src/components/WarningBanner.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 
 const WarningBanner: FC = () => {
   return (
-    <div className="bg-[#FF486E] font-semibold text-sm text-white py-2 z-[9999999]">
-        <div className="overflow-hidden">
+    <div className="bg-[#FF486E] font-semibold text-sm text-white text-center py-2 z-[9999999]">
+      <div className="overflow-hidden">
         <div className="w-[5000px] flex items-center">
           <div className="w-[1000px] inline-flex items-center justify-center">
             <span>The dYdX testnet (dydx-testnet-1) is currently shut down in preparation for Testnet 2. The data is frozen until Testnet 2 launches.</span>
