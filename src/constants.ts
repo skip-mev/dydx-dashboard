6b18caaaa1e2bc10bd616c882b294c197a0a5e10
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-09-21 20:42:41 +0200

    fix: switch datapoint limit to 5000

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://dydx-mev-api-dev.skip.money";
-export const MAIN_CHART_DATAPOINT_LIMIT = 1000;
+export const MAIN_CHART_DATAPOINT_LIMIT = 5000;
 export const MAIN_CHART_DATAPOINT_EVERY = 5;
 export const PROBABILITY_THRESHOLD = 0.67;
