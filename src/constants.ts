79f576d3e83e5b39894d86a60062799097a3bcb6
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-09-21 20:42:07 +0200

    fix: remove merge conflict

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://dydx-mev-api-dev.skip.money";
-=======
 export const MAIN_CHART_DATAPOINT_LIMIT = 1000;
 export const MAIN_CHART_DATAPOINT_EVERY = 5;
 export const PROBABILITY_THRESHOLD = 0.67;
->>>>>>> b3d1bea (feat: use cumulative mev query instead of raw_mev)
