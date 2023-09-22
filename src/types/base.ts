9b8068faf9d97d54c2c4f9e0cb388f5e78af9ea9
Griko Nibras <griko@nibras.co>
2023-09-23 01:57:48 +0700

    feat: extract api types

---
+++ src/types/base.ts
@@ -0,0 +1,34 @@
+export type CumulativeDatapoint = {
+  height: string;
+  value: number;
+};
+
+export type Datapoint = {
+  height: string;
+  probability: number;
+  proposer: string;
+  value: number;
+};
+
+export type MainChartData = {
+  validator: string;
+  cumulativeMev: {
+    key: number;
+    value: number;
+  }[];
+};
+
+export type Validator = {
+  moniker: string;
+  pubkey: string;
+  stake: string;
+};
+
+export type ValidatorWithStats = Validator & {
+  averageMev: number;
+};
+
+export type ValidatorStats = {
+  averageMev: string;
+  validatorPubkey: string;
+};
