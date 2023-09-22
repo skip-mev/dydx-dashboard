11e880d7ad0c667259163b314e61692ddb522c23
Griko Nibras <griko@nibras.co>
2023-09-23 01:55:44 +0700

    chore: move queryClient to lib dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { queryClient } from "@/query";
+import { queryClient } from "@/lib/react-query";
 import "@/styles/globals.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
