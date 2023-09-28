de6950ece5473986ccecbe51157901289462ba91
Griko Nibras <griko@nibras.co>
2023-09-29 03:25:47 +0700

    feat: create MainChartToggles component

---
+++ src/components/pages/home/MainChartToggles.tsx
@@ -0,0 +1,52 @@
+import { useValidatorsWithStatsQuery } from "@/hooks";
+import {
+  addSelectedValidator,
+  removeSelectedValidator,
+  useHomeStore,
+} from "@/store/home";
+import { ComponentProps } from "react";
+
+export const MainChartToggles = (props: ComponentProps<"div">) => {
+  const blocks = useHomeStore((state) => state.blocks);
+  const hideInactive = useHomeStore((state) => state.hideInactive);
+  const selected = useHomeStore((state) => state.selectedValidators);
+
+  const { data: validators = [] } = useValidatorsWithStatsQuery({
+    blocks,
+    select: (arr = []) => {
+      if (hideInactive) {
+        arr = arr.filter((validator) => validator.stake !== "0");
+      }
+      return arr;
+    },
+  });
+
+  return (
+    <div {...props}>
+      {validators?.map((validator) => (
+        <button
+          className={`${
+            selected.includes(validator)
+              ? "bg-[#b51717]"
+              : "bg-white/5 hover:bg-[#b51717]"
+          } text-xs py-1 px-2 rounded-md transition-colors`}
+          key={validator.pubkey}
+          onMouseOver={() =>
+            useHomeStore.setState({
+              highlightedValidator: validator,
+            })
+          }
+          onClick={() => {
+            if (selected.includes(validator)) {
+              removeSelectedValidator(validator);
+            } else {
+              addSelectedValidator(validator);
+            }
+          }}
+        >
+          {validator.moniker}
+        </button>
+      ))}
+    </div>
+  );
+};
