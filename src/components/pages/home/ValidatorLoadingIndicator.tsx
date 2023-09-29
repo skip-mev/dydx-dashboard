90482673fbe96e00b7ef399a43950676f94ca42b
Griko Nibras <griko@nibras.co>
2023-09-30 06:04:02 +0700

    chore: set loading indicator z-index

--- src/components/pages/home/ValidatorLoadingIndicator.tsx
+++ src/components/pages/home/ValidatorLoadingIndicator.tsx
@@ -8,7 +8,7 @@ export const ValidatorLoadingIndicator = () => {
   if (!isValidatorsLoading) return null;
 
   return (
-    <div className="w-full h-1 bg-indigo-500 fixed top-0 left-0 right-0 overflow-hidden">
+    <div className="w-full h-1 bg-indigo-500 fixed top-0 left-0 right-0 overflow-hidden z-50">
       <div className="bg-indigo-600 w-full h-full animate-fade-right animate-infinite" />
     </div>
   );
