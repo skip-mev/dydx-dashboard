0e4c57da4d359e81cb67baeac0ceccb0b93cbea6
Griko Nibras <griko@nibras.co>
2023-09-30 06:05:41 +0700

    chore: center navbar on small viewports

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 const NavBar: FC = () => {
   return (
     <nav className="p-4">
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg flex justify-center md:justify-start mx-auto">
         <img className="h-9" src="/skip-logo.svg" alt="skip-logo" />
       </div>
     </nav>
