6cd4187ea91817a02dd29759d9ae2a397f0da8e4
Griko Nibras <griko@nibras.co>
2023-10-01 00:54:42 +0700

    feat: aggressive optimize chart toggles

--- src/components/pages/home/MainChartToggles.tsx
+++ src/components/pages/home/MainChartToggles.tsx
@@ -5,13 +5,14 @@ import {
   toggleSelectedMoniker,
   useHomeStore,
 } from "@/store/home";
+import { Validator } from "@/types/base";
 import { ValidatorComparer } from "@/types/utils";
-import { ComponentProps, useEffect } from "react";
+import clsx from "clsx";
+import { ComponentProps, memo, useEffect } from "react";
 
 export const MainChartToggles = (props: ComponentProps<"div">) => {
   const blocks = useHomeStore((state) => state.blocks);
   const hideInactive = useHomeStore((state) => state.hideInactive);
-  const selected = useHomeStore((state) => state.selectedMonikers);
 
   const { data: validators = [] } = useValidatorsWithStatsQuery({
     blocks,
@@ -24,27 +25,50 @@ export const MainChartToggles = (props: ComponentProps<"div">) => {
   });
 
   useEffect(() => {
-    if (validators?.[0] && Object.keys(selected).length < 1) {
-      resetSelectedMoniker([validators[0]]);
-    }
-  }, [selected, validators]);
+    const unsub = useHomeStore.subscribe(
+      (state) => state.selectedMonikers,
+      (selected) => {
+        if (validators?.[0] && Object.keys(selected).length === 0) {
+          resetSelectedMoniker([validators[0]]);
+        }
+      },
+      {
+        fireImmediately: true,
+      }
+    );
+    return unsub;
+  }, [validators]);
 
   return (
     <div {...props}>
       {validators?.map((validator) => (
-        <button
-          className={`${
-            selected[validator.moniker]
-              ? "bg-[#b51717]"
-              : "bg-white/5 hover:bg-[#b51717]"
-          } text-xs py-1 px-2 rounded-md transition-colors`}
-          key={validator.pubkey}
-          onMouseOver={() => setHighlightedMoniker(validator)}
-          onClick={() => toggleSelectedMoniker(validator)}
-        >
-          {validator.moniker}
-        </button>
+        <ToggleMoniker key={validator.pubkey} moniker={validator.moniker} />
       ))}
     </div>
   );
 };
+
+const ToggleMoniker = memo(
+  function ToggleMoniker({ moniker }: Pick<Validator, "moniker">) {
+    const highlighted = useHomeStore((state) => state.highlightedMoniker);
+    const selected = useHomeStore((state) => state.selectedMonikers);
+    return (
+      <button
+        className={clsx(
+          "text-xs py-1 px-2 rounded-md",
+          "bg-white/5 hover:bg-[#b51717]",
+          "data-[highlighted=true]:bg-[#34f1ff84]",
+          "data-[selected=true]:bg-[#b51717]"
+        )}
+        onClick={() => toggleSelectedMoniker({ moniker })}
+        onMouseEnter={() => setHighlightedMoniker({ moniker })}
+        onMouseLeave={() => setHighlightedMoniker(undefined)}
+        data-highlighted={highlighted === moniker}
+        data-selected={selected[moniker]}
+      >
+        {moniker}
+      </button>
+    );
+  }
+  //
+);
