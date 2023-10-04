d5f4e966ee611bc567322d7b8b6f8b1716f3d9f5
Griko Nibras <griko@nibras.co>
2023-10-04 22:13:19 +0300

    feat: extract svg icons

---
+++ src/components/icons/Dydx.tsx
@@ -0,0 +1,79 @@
+import { ComponentProps } from "react";
+
+export const DydxIcon = (props: ComponentProps<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 97 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clip-path="url(#prefix__clip0_172_852)">
+        <path d="M89.248 0L68.495 29.732h6.372L95.727 0h-6.479z" fill="#fff" />
+        <path
+          d="M75.53 0l6.107 8.761-3.186 4.779L69.026 0h6.505z"
+          fill="url(#prefix__paint0_linear_172_852)"
+        />
+        <path
+          d="M89.868 29.735l-6.77-9.69 3.186-4.647 9.955 14.337h-6.371z"
+          fill="url(#prefix__paint1_linear_172_852)"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M61.062 0h5.575v29.732h-5.575v-1.11A11.29 11.29 0 0155.637 30c-6.241 0-11.301-5.037-11.301-11.25s5.06-11.25 11.3-11.25c1.967 0 3.815.5 5.426 1.378V0zm-5.485 25.062c3.535 0 6.401-2.852 6.401-6.371 0-3.52-2.866-6.372-6.4-6.372-3.536 0-6.401 2.853-6.401 6.372 0 3.519 2.865 6.371 6.4 6.371z"
+          fill="#fff"
+        />
+        <path d="M44.89 0L24.16 29.732h6.499L51.37 0h-6.48z" fill="#fff" />
+        <path
+          d="M30.663 0l6.505 9.027-3.584 5.044L23.894 0h6.77z"
+          fill="url(#prefix__paint2_linear_172_852)"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M16.726 0H22.3v29.732h-5.575v-1.11A11.29 11.29 0 0111.3 30C5.06 30 0 24.963 0 18.75S5.06 7.5 11.301 7.5c1.966 0 3.815.5 5.425 1.378V0zm-5.484 25.062c3.534 0 6.4-2.852 6.4-6.371 0-3.52-2.865-6.372-6.4-6.372S4.84 15.172 4.84 18.691c0 3.519 2.866 6.371 6.4 6.371z"
+          fill="#fff"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="prefix__paint0_linear_172_852"
+          x1={74.071}
+          y1={1.858}
+          x2={82.812}
+          y2={12.411}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#fff" />
+          <stop offset={1} stop-color="#fff" stop-opacity={0.55} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__paint1_linear_172_852"
+          x1={91.328}
+          y1={27.478}
+          x2={80.588}
+          y2={12.999}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#6966FF" />
+          <stop offset={1} stop-color="#6966FF" stop-opacity={0.36} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__paint2_linear_172_852"
+          x1={29.203}
+          y1={1.931}
+          x2={38.265}
+          y2={13.012}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#fff" />
+          <stop offset={1} stop-color="#fff" stop-opacity={0.55} />
+        </linearGradient>
+        <clipPath id="prefix__clip0_172_852">
+          <path fill="#fff" d="M0 0h96.372v30H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
