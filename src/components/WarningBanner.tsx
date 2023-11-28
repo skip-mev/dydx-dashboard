35c7878f1523e2fa8f5b53e73ef3036883f33c51
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-11-28 18:49:27 +0100

    feat: add warning banner for data inaccuracies

---
+++ src/components/WarningBanner.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const WarningBanner: FC = () => {
+  return (
+    <div className="bg-[#FF486E] font-semibold text-sm text-white items-center text-center py-2 z-[9999999]">
+      <div className="flex items-center justify-center">
+        <div className="inline-flex items-center justify-center">
+          <span>Data on this dashboard will not be accurate until traders fully transition to dYdX v4.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WarningBanner;
