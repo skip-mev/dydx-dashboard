9f46d2c6b153d72874a5fd24f8300ecbad86e345
Griko Nibras <griko@nibras.co>
2023-12-04 23:41:23 +0700

    chore: remove hardcoded sentry envs

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,12 +4,8 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    "https://8c14a825146b8bd474baf194a5d7fc50@o4504768725909504.ingest.sentry.io/4505624305139712",
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
