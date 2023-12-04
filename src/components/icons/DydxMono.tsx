cf3a150db9676aee12368b24731381321a57c08b
Griko Nibras <griko@nibras.co>
2023-12-04 23:28:53 +0700

    fix: update invalid svg props

--- src/components/icons/DydxMono.tsx
+++ src/components/icons/DydxMono.tsx
@@ -8,7 +8,7 @@ export const DydxMonoIcon = (props: ComponentProps<"svg">) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#prefix__clip0_172_852_mono)">
+      <g clipPath="url(#prefix__clip0_172_852_mono)">
         <path
           d="M89.248 0L68.495 29.732h6.372L95.727 0h-6.479z"
           fill="currentColor"
@@ -22,8 +22,8 @@ export const DydxMonoIcon = (props: ComponentProps<"svg">) => {
           fill="url(#prefix__paint1_linear_172_852_mono)"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M61.062 0h5.575v29.732h-5.575v-1.11A11.29 11.29 0 0155.637 30c-6.241 0-11.301-5.037-11.301-11.25s5.06-11.25 11.3-11.25c1.967 0 3.815.5 5.426 1.378V0zm-5.485 25.062c3.535 0 6.401-2.852 6.401-6.371 0-3.52-2.866-6.372-6.4-6.372-3.536 0-6.401 2.853-6.401 6.372 0 3.519 2.865 6.371 6.4 6.371z"
           fill="currentColor"
         />
@@ -36,8 +36,8 @@ export const DydxMonoIcon = (props: ComponentProps<"svg">) => {
           fill="url(#prefix__paint2_linear_172_852_mono)"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M16.726 0H22.3v29.732h-5.575v-1.11A11.29 11.29 0 0111.3 30C5.06 30 0 24.963 0 18.75S5.06 7.5 11.301 7.5c1.966 0 3.815.5 5.425 1.378V0zm-5.484 25.062c3.534 0 6.4-2.852 6.4-6.371 0-3.52-2.865-6.372-6.4-6.372S4.84 15.172 4.84 18.691c0 3.519 2.866 6.371 6.4 6.371z"
           fill="currentColor"
         />
